ed5ea9e8ec1e997aba871bf1953a840eaef181d5
John Cinq-Mars <jcinqmars2000@yahoo.com>
2019-01-22 11:37:33 -0500

    Initial Commit

---
+++ src/test/java/guru/springframework/JavaHelloWorldTest.java
@@ -0,0 +1,13 @@
+package guru.springframework;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JavaHelloWorldTest {
+
+    @Test
+    public void getHello() {
+        System.out.println("junit 4 test");
+    }
+}
